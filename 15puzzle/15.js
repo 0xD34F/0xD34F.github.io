b7202d82aeadbaf2ced1e9ae9a82440137fb5b65
Victor <x89168419357@gmail.com>
2017-01-05 04:38:34 +0300

    15puzzle added

---
+++ 15puzzle/15.js
@@ -0,0 +1,203 @@
+function getRandomInt(max, min) {
+    min = min || 0;
+
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+var puzzle = {
+    tiles: [],
+    solutions: [
+        [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, null ],
+        [ null, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 ],
+        [ 13, 9, 5, 1, 14, 10, 6, 2, 15, 11, 7, 3, null, 12, 8, 4 ]
+    ],
+    gameStarted: false,
+    gamePane: null,
+    initGamePane: function() {
+        for (var i = 0; i < 15; i++) {
+            var x = i % 4,
+                y = Math.floor(i / 4);
+
+            var t = document.createElement('div');
+            t.classList.add('tile');
+            t.style.left = x * 100;
+            t.style.top = y * 100;
+            t.innerHTML = '' + (i + 1);
+            t.tileData = (i + 1);
+
+            puzzle.gamePane.appendChild(t);
+            puzzle.tiles.push(t);
+        }
+
+        puzzle.emptyCoord = 16;
+        puzzle.tiles.push(null);
+    },
+    getTileByIndex: function(index) {
+        var c = puzzle.gamePane.children;
+        for (var i = 0; i < c.length; i++) {
+            var tile = c[i];
+            if (tile.tileData === index) {
+                return tile;
+            }
+        }
+
+        return null;
+    },
+    startGame: function() {
+        puzzle.endGameMessage(false);
+        puzzle.gameStarted = true;
+
+        var tiles = [ null ];
+        for (var i = 1; i < 16; i++) {
+            tiles.push(i);
+        }
+
+        var data = [];
+        for (i = 0; i < 16; i++) {
+            data.push({
+                tile: puzzle.getTileByIndex(tiles.splice(getRandomInt(tiles.length), 1)[0]),
+                coord: i
+            })
+        }
+        puzzle.moveTiles(data, true);
+    },
+    endGameMessage: (function() {
+        var t = document.createElement('div');
+        t.classList.add('tile', 'control');
+        t.style.width = 230;
+        t.style.left = 75;
+        t.style.top = 150;
+        t.style.position = 'absolute';
+        t.innerHTML = 'DONE!';
+
+        return function(show) {
+            if (show) {
+                puzzle.gamePane.appendChild(t);
+            } else {
+                var c = puzzle.gamePane.childNodes;
+                for (var i = 0; i < c.length; i++) {
+                    if (c[i] === t) {
+                        puzzle.gamePane.removeChild(t);
+                        break;
+                    }
+                }
+            }
+        };
+    })(),
+    endGame: function() {
+        puzzle.gameStarted = false;
+        puzzle.endGameMessage(true);
+    },
+    isSolved: function() {
+        var currentSolution = puzzle.tiles.map(function(n) {
+            return n ? n.tileData : null;
+        });
+
+nextSolution:
+        for (var i = 0; i < puzzle.solutions.length; i++) {
+            var s = puzzle.solutions[i];
+            for (var j = 0; j < s.length; j++) {
+                if (s[j] !== currentSolution[j]) {
+                    continue nextSolution;
+                }
+            }
+
+            return true;
+        }
+
+        return false;
+    },
+    moveTiles: function(data, slow) {
+        if (slow) {
+            puzzle.gamePane.classList.add('restarting');
+            setTimeout(function() {
+                puzzle.gamePane.classList.remove('restarting');
+            }, 400);
+        }
+
+        data.forEach(function(n) {
+            puzzle.tiles[n.coord] = n.tile;
+
+            if (n.tile === null) {
+                puzzle.emptyCoord = n.coord;
+            } else {
+                n.tile.style.left = (n.coord % 4) * 100;
+                n.tile.style.top = Math.floor(n.coord / 4) * 100;
+            }
+        });
+    },
+    makeStep: function(tile, coord) {
+        if (!tile || puzzle.tiles[coord] !== tile || Math.abs(coord % 4 - puzzle.emptyCoord % 4) === 3) {
+            return false;
+        }
+
+        puzzle.moveTiles([ {
+            tile: tile,
+            coord: puzzle.emptyCoord
+        }, {
+            tile: null,
+            coord: coord
+        } ]);
+
+        if (puzzle.isSolved()) {
+            puzzle.endGame();
+        }
+
+        return true;
+    }
+};
+
+window.onload = function() {
+    document.getElementById('restart').onclick = function(e) {
+        puzzle.startGame();
+    };
+
+    document.getElementById('solve').onclick = function(e) {
+        puzzle.gameStarted = false;
+        puzzle.moveTiles(puzzle.tiles.map(function(n, i) {
+            return {
+                tile: puzzle.getTileByIndex(i + 1),
+                coord: i
+            };
+        }), true);
+    };
+
+    puzzle.gamePane = document.getElementById('gamePane');
+    puzzle.gamePane.onclick = function(e) {
+        var t = e.target;
+        if (t.classList.contains('tile')) {
+            if (puzzle.gameStarted) {
+                puzzle.makeStep(t, puzzle.emptyCoord - 1) ||
+                puzzle.makeStep(t, puzzle.emptyCoord + 1) ||
+                puzzle.makeStep(t, puzzle.emptyCoord - 4) ||
+                puzzle.makeStep(t, puzzle.emptyCoord + 4);
+            } else {
+                var restart = document.getElementById('restart');
+                restart.classList.add('border-blink');
+                setTimeout(function() {
+                    restart.classList.remove('border-blink');
+                }, 1000);
+            }
+        }
+    };
+
+    puzzle.initGamePane();
+    setTimeout(puzzle.startGame, 500);
+};
+
+window.onkeydown = function(e) {
+    if (puzzle.gameStarted) {
+        var coord = puzzle.emptyCoord;
+
+        switch (e.which) {
+            case 37: coord += 1; break;
+            case 38: coord += 4; break;
+            case 39: coord -= 1; break;
+            case 40: coord -= 4; break;
+
+            default: return;
+        }
+
+        puzzle.makeStep(puzzle.tiles[coord], coord);
+    }
+};
